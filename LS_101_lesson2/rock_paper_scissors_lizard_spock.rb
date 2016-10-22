798ec76806173f30f1d0ceb0cfb91c3a8d4a9935
CodeNeophyte <thecodeneophyte@gmail.com>
2016-10-21 23:23:38 -1000

    Made changes to RPSLK based on initial code review

--- LS_101_lesson2/rock_paper_scissors_lizard_spock.rb
+++ LS_101_lesson2/rock_paper_scissors_lizard_spock.rb
@@ -1,6 +1,17 @@
 # variables
+DISPLAY_CHOICES = %w((R)ock (P)aper (S)cissors (L)izard Spoc(K))
 VALID_CHOICES = %w(rock paper scissors lizard spock)
-choice = ''
+VALID_WINS = { rock: %w(scissors lizard),
+               paper: %w(rock spock),
+               scissors: %w(paper lizard),
+               lizard: %w(spock paper),
+               spock: %w(scissors rock) }
+CONVERTED_INPUT = { r: "rock",
+                    p: "paper",
+                    s: "scissors",
+                    l: "lizard",
+                    k: "spock" }
+user_choice = ''
 player_score = 0
 computer_score = 0
 tie_score = 0
@@ -12,39 +23,23 @@ def prompt(message)
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper') ||
-    (first == 'rock' && second == 'lizard') ||
-    (first == 'lizard' && second == 'spock') ||
-    (first == 'spock' && second == 'scissors') ||
-    (first == 'scissors' && second == 'lizard') ||
-    (first == 'lizard' && second == 'paper') ||
-    (first == 'paper' && second == 'spock') ||
-    (first == 'spock' && second == 'rock')
+  VALID_WINS[first.to_sym].include?(second)
 end
 
 def convert_input(input)
-  case input.downcase()
-  when 'r'
-    return "rock"
-  when 'p'
-    return "paper"
-  when 'l'
-    return "lizard"
-  when 's'
-    loop do
-      puts "Type 'c' for scissors or 'p' spock "
-      convert_answer = Kernel.gets().chomp()
-      if convert_answer == "c"
-        return "scissors"
-      elsif convert_answer == "p"
-        return "spock"
-      else
-        puts "Invalid entry."
-      end
-    end
+  CONVERTED_INPUT.fetch(input.downcase().to_sym)
+end
+
+def user_input
+  choice = ''
+  loop do # user choice loop
+    prompt("Choose one: #{DISPLAY_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp().downcase()
+    break if CONVERTED_INPUT.has_key?(choice.to_sym)
+    prompt("That's not a valid choice.")
   end
+  #puts choice
+  choice
 end
 
 def display_results(player, computer)
@@ -57,48 +52,57 @@ def display_results(player, computer)
   end
 end
 
-def tally_score(player, computer)
+def add_score(player, computer)
   if win?(player, computer)
-    return "player"
+    "player"
   elsif win?(computer, player)
-    return "computer"
+    "computer"
   else
-    return "tie"
+    "tie"
   end
 end
 
 def check_winner(score_player, score_computer)
   if score_player == 5
-    return "player"
+    "player"
   elsif score_computer == 5
-    return "computer"
+    "computer"
   else
-    return "continue"
+    "continue"
   end
+end
+
+def clear_screen
+  system('clear') || system('cls')
+end
+
+def continue_playing?
+  answer = ''
+  loop do
+    prompt("Do you want to play again? (Y) for yes/(N) for no")
+    answer = Kernel.gets().chomp().downcase()
+    break unless answer != 'y' && answer != 'n'
+    prompt("That's an invalid input.")
+  end
+  clear_screen
+  answer == 'y' ? true : false
 end
 
 # main program
 loop do # main loop
   loop do # round loop
-    loop do # user choice loop
-      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-      choice = Kernel.gets().chomp()
-      choice = convert_input(choice)
-
-      if VALID_CHOICES.include?(choice)
-        break
-      else
-        prompt("That's not a valid choice.")
-      end
-    end
+    user_choice = convert_input(user_input)
+    #puts user_choice
 
     computer_choice = VALID_CHOICES.sample
 
-    prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+    clear_screen
 
-    display_results(choice, computer_choice)
+    prompt("You chose: #{user_choice}; Computer chose: #{computer_choice}")
 
-    scoring = tally_score(choice, computer_choice)
+    display_results(user_choice, computer_choice)
+
+    scoring = add_score(user_choice, computer_choice)
     if scoring == "player"
       player_score += 1
     elsif scoring == "computer"
@@ -107,8 +111,9 @@ loop do # main loop
       tie_score += 1
     end
 
-    prompt("Your score is: #{player_score}; The computer's score:
-            #{computer_score}; Tie score is: #{tie_score}")
+    prompt("Your score is: #{player_score}
+    The computer's score: #{computer_score}
+    Tie score is: #{tie_score}")
 
     winner = check_winner(player_score, computer_score)
     if winner == "player"
@@ -117,15 +122,10 @@ loop do # main loop
     elsif winner == "computer"
       prompt("The Computer won the game!")
       break
-    else
-      # continue game
-      next
     end
   end
 
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  break unless continue_playing?
   # reset score variables to zero
   player_score = 0
   computer_score = 0
